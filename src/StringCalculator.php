3fc4ca0341e6f0c4085ef4aeece8778570eb9e57
Alejandro Sola <alejandrosolaalba@gmail.com>
2025-03-25 11:54:43 +0100

    Test 6 Calculator Works

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -5,4 +5,67 @@ namespace Deg540\StringCalculatorPHP;
 class StringCalculator
 {
     // TODO: String Calculator Kata
+
+
+    /**
+     * @param string $string_numbers
+     *
+     * @return int
+     */
+    function Add(string $string_numbers): int {
+        if(strlen($string_numbers) <1) {
+            return 0;
+        }elseif (strlen($string_numbers) == 1) {
+            return intval($string_numbers);
+        }else{
+            $splitedArgs = $this->getSplitArgs($string_numbers);
+            return $this->sumStringArguments($splitedArgs);
+        }
+    }
+
+    /**
+     * @param string $string_numbers
+     *
+     * @return array
+     */
+    function getSplitArgs(string $string_numbers): array
+    {
+        if($string_numbers[0] == '/'){
+            return preg_split('/[\n'. $this->getDeliminator($string_numbers) . ']+/', $this->getNumbers($string_numbers));
+        }
+        return preg_split('/[\n,]+/', $string_numbers);
+    }
+
+    /**
+     * @param string $string_numbers
+     *
+     * @return string
+     */
+    function getDeliminator(string $string_numbers): string
+    {
+        return substr(explode("\n", $string_numbers,2)[0], 2);
+
+    }
+
+    /**
+     * @param string $string_numbers
+     *
+     * @return string
+     */
+    function getNumbers(string $string_numbers): string
+    {
+        return explode("\n", $string_numbers,2)[1];
+    }
+
+    /**
+     * @param array $splitedArgs
+     * @return int
+     */
+    function sumStringArguments(array $splitedArgs): int {
+        $suma = 0;
+        foreach ($splitedArgs as $arg) {
+            $suma += intval($arg);
+        }
+        return $suma;
+    }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -10,4 +10,65 @@ use PHPUnit\Framework\TestCase;
 final class StringCalculatorTest extends TestCase
 {
     // TODO: String Calculator Kata Tests
+    private StringCalculator $stringCalculator;
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->stringCalculator = new StringCalculator();
+    }
+
+
+    /**
+     * @test
+     */
+    public function givenEmptyStringReturns0() : void
+    {
+        $returnValue = $this->stringCalculator->Add("");
+        $this->assertEquals(0,$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function given1ArgumentReturnsArgument() : void
+    {
+        $returnValue = $this->stringCalculator->Add("1");
+        $this->assertEquals(1,$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function given1And2AsArgumentsReturns3() : void
+    {
+        $returnValue = $this->stringCalculator->Add("1,2");
+        $this->assertEquals(3,$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function givenNArgumentStringReturnsSum() : void
+    {
+        $returnValue = $this->stringCalculator->Add("1,2,3,4,5,6");
+        $this->assertEquals(21,$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function givenArgumentLineBreakFunctionWorks() : void
+    {
+        $returnValue = $this->stringCalculator->Add("1\n2,3,4,5,6");
+        $this->assertEquals(21,$returnValue);
+    }
+
+    /**
+     * @test
+     */
+    public function givenDeliminatorFunctionWorks() : void
+    {
+        $returnValue = $this->stringCalculator->Add("//;\n1;2");
+        $this->assertEquals(3,$returnValue);
+    }
 }
